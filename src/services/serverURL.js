5fcff9b8a24ec7c6b2ea53f4d3aa03db62cd561a
Anisha Soman <47136210+anishasoman@users.noreply.github.com>
2026-02-28 12:08:45 +0530

    Update serverURL.js

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,2 +1,2 @@
-export const serverURL = "http://localhost:3000";
-// export const serverURL = "https://farmfund-backendcode.onrender.com"
+// export const serverURL = "http://localhost:3000";
+export const serverURL = "https://farmfund-backendcode.onrender.com"
